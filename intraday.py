a88d4cef8826b3c94b179733eb10aa29ccb10754
Marcus Schiesser <mail@marcusschiesser.de>
2020-01-12 11:26:39 +0800

    initial commit

---
+++ intraday.py
@@ -0,0 +1,51 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime, date, timedelta, timezone
+
+# there's a limit from yfinance to only get intraday date for the last 30 days
+START_DATE = date.today() - timedelta(days=30)
+
+def to_utc(d):
+    return d.tz_convert(timezone.utc)
+
+def df_to_utc(df):
+    df.index = df.index.map(to_utc)
+    
+def get_tickerfile(ticker):
+    dirname = os.path.dirname(__file__)
+    return dirname + '/data/' + ticker + '.csv'
+
+def get_cache(ticker):
+    filename = get_tickerfile(ticker)
+    try:
+        return pd.read_csv(filename, parse_dates=True, index_col='Datetime')
+    except FileNotFoundError:
+        print("Ticker cache file '{}' not found.\n".format(filename))
+        # return empty data frame on error
+        return pd.DataFrame()
+
+def get_lastday(df):
+    if len(df) == 0:
+        return START_DATE
+    df['temp'] = pd.to_datetime(df.index)
+    max = df['temp'].max()
+    df.drop(columns='temp', inplace=True)
+    return max.date()
+    
+def get_ticker(ticker):
+    old_df = get_cache(ticker)
+    start_date = get_lastday(old_df) + timedelta(days=1)  
+    end_date = start_date + timedelta(days=7)  
+    print("getting ticker {} from {} to {}".format(ticker, start_date, end_date))
+    # get new data
+    t = yf.Ticker(ticker)
+    df = t.history(start=start_date, end=end_date, interval="1m")
+    # append new data
+    old_df = old_df.append(df, sort=False)
+    # serialize to CSV
+    old_df.to_csv(get_tickerfile(ticker))
+    # return latest data as UTC
+    df_to_utc(old_df)
+    return old_df
---
+++ test_intraday.py
@@ -0,0 +1,24 @@
+import intraday
+import pandas as pd
+import os
+
+TEST_TICKER = "__test__"
+
+def test_get_lastday_emptyframe():
+    df = pd.DataFrame(columns = ['Datetime'])
+    assert intraday.get_lastday(df) == intraday.START_DATE, "empty dataframe must return start date"
+
+def test_get_lastday():
+    df = intraday.get_cache(TEST_TICKER)
+    date = intraday.get_lastday(df)
+    assert date == date.fromisoformat("2019-11-20"), "test dataframe must return 2019-11-20"
+
+def test_ticker():
+    df = intraday.get_ticker('SPY')
+    print(df.head())
+
+if __name__ == "__main__":
+    test_get_lastday_emptyframe()
+    test_get_lastday()
+    test_ticker()
+    print("Everything passed")
